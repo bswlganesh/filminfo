b59099f04ed5f5821653eb5e622a06d12e49e55d
Baswal Ganesh <baswalganesh1@gmail.com>
2025-09-05 21:06:30 +0530

    added scrool to top functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Header,Footer} from './components'
 //import { MovieDetails} from './pages'
+import ScrollToTop from './components/ScrollToTop'
 import {AllRoutes} from './routes/AllRoutes'
 export default function App() {
   return (
     <>              
       <Header/>
       <main>
+         <ScrollToTop />
           <AllRoutes />
         </main>
       
