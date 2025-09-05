030e123528458ffc5fe9d2fb34b2adb9ba57621a
Baswal Ganesh <baswalganesh1@gmail.com>
2025-09-05 20:48:04 +0530

    added seaching loading

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,24 +5,31 @@ import useFetch from '../hooks/useFetch'
 
 
 export default function SearchPage() {
-  
+ 
   const [searchParam]=useSearchParams()
   const queryTerm=searchParam.get('s')
  
    const apiUrl = `https://www.omdbapi.com/?apikey=2a6b8207&s=${queryTerm}`;
-  const {data:movies}=useFetch({ url: apiUrl })
+   const { data: movies, isLoading } = useFetch({ url: apiUrl });
   return (
-    <main>
-      <p className="text-3xl text-gray-700 dark:text-white">
-          { movies.length === 0 ? `No result found for '${queryTerm}'` : `Results for '${queryTerm}'` }
-        </p>
-          <section className='card'>
-            {movies.map((movie)=>(
-               <MovieCard key={movie.imdbID} movie={movie}/>
-            ))}
-           
-          
+        <main>
+      {/* 2. Conditionally render a loading message or the results */}
+      { isLoading ? (
+        <p className="text-3xl text-gray-700 dark:text-white animate-pulse">Searching...</p>
+      ) : (
+        <>
+          <p className="text-3xl text-gray-700 dark:text-white">
+            { movies.length === 0 ? `No result found for '${queryTerm}'` : `Results for '${queryTerm}'` }
+          </p>
+          <section className='max-w-7xl mx-auto py-7'>
+            <div className='flex justify-start flex-wrap'>
+              {movies.map((movie) => (
+                <MovieCard key={movie.imdbID} movie={movie} />
+              ))}
+            </div>
           </section>
-        </main>
+        </>
+      )}
+    </main>
   )
 }
